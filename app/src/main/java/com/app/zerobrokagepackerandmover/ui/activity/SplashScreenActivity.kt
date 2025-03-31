15328174f548a16b15462b061d69ae713a01948e
Anupriyanshu kushwaha <anp.flybizz@gmail.com>
2025-03-31 18:04:51 +0530

    First Commit

---
+++ app/src/main/java/com/app/zerobrokagepackerandmover/ui/activity/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.app.zerobrokagepackerandmover.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.app.zerobrokagepackerandmover.R
+import com.app.zerobrokagepackerandmover.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
