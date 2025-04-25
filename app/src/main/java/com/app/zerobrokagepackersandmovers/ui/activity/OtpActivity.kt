a8dbe493f52cc83e5191d76b06ea3869968acdf6
Anupriyanshu kushwaha <anp.flybizz@gmail.com>
2025-04-25 18:15:16 +0530

    Add timer

--- app/src/main/java/com/app/zerobrokagepackersandmovers/ui/activity/OtpActivity.kt
+++ app/src/main/java/com/app/zerobrokagepackersandmovers/ui/activity/OtpActivity.kt
@@ -2,6 +2,7 @@ package com.app.zerobrokagepackersandmovers.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.app.zerobrokagepackersandmovers.MainActivity
@@ -9,6 +10,8 @@ import com.app.zerobrokagepackersandmovers.databinding.ActivityOtpBinding
 
 class OtpActivity : AppCompatActivity() {
     private lateinit var binding: ActivityOtpBinding
+    private lateinit var countDownTimer: CountDownTimer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOtpBinding.inflate(layoutInflater)
@@ -31,6 +34,28 @@ class OtpActivity : AppCompatActivity() {
                 Toast.makeText(this, "Please enter OTP", Toast.LENGTH_SHORT).show()
             }
         }
+
+        binding.tvResend.setOnClickListener {
+            startTimer()
+        }
+
+        startTimer()
+    }
+
+    private fun startTimer() {
+        binding.tvResend.isEnabled = false
+        countDownTimer = object : CountDownTimer(16000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val seconds = (millisUntilFinished / 1000) % 60
+                val minutes = (millisUntilFinished / 1000) / 60
+                binding.timer.text = String.format("%02d:%02d", minutes, seconds)
+            }
+
+            override fun onFinish() {
+                binding.timer.text = "Done!"
+                binding.tvResend.isEnabled = true
+            }
+        }.start()
     }
 
 }
